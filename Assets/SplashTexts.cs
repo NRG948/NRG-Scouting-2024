25d1d7cbd80502e63f7a9008778a631eb5f0f525
GuestUser1000101 <ryanzou2008@gmail.com>
2024-02-13 19:56:14 -0800

    Add splash text to menu.

---
+++ Assets/SplashTexts.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SplashTexts : MonoBehaviour
+{
+    public TMP_Text txt;
+
+    public string[] splashTexts = {
+        "Watt was that?",
+        "Shocking!",
+        "Have fun, darnit!",
+        "Red Alliance!",
+        "Blue Alliance!",
+        "RIP Dodo",
+        "No comms?",
+        "MARGINS >:(",
+        "Electric!",
+        "Ayo pay attention",
+        "Shift + Alt + F your pit",
+        "Who are we? MC^2",
+        "kg m^2/s^2",
+        "Not by the wires!",
+        "Have you turned it off and on again?",
+        "The only thing I can pull are my commits",
+        "Sushi Wannabe",
+        "99.1% Pure",
+        "Red and Gold!",
+        "Dark mode when",
+        "FIRE IN THE HOLE",
+        "Sugar free",
+        "If there are joysticks, are there sadsticks?",
+        "Gulati approved",
+        "In-Game Purchases Included",
+        "Drop that like and leave a comment below",
+        "It's not a bug, it's a feature!",
+        "Give us a better name",
+        "W IT",
+        "C#? It looks rather dull to me",
+        "Programmer? I'm glad you support proper English",
+        "And that's how I lost my license",
+        "Zero factor authentication",
+        "Accept cookies? \\(•ω•`)o",
+        "error 1002: ; Expected",
+    };
+
+    void Start()
+    {
+        txt.text = splashTexts[Random.Range(0, splashTexts.Length)];
+    }
+}
