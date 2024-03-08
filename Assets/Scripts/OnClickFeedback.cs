344b0ead577da0d030f4ad608e89936918ded466
101zh <67253838+101zh@users.noreply.github.com>
2024-03-07 16:37:57 -0800

    work on haptic feedback in data viewer

--- Assets/Scripts/OnClickFeedback.cs
+++ Assets/Scripts/OnClickFeedback.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class IndicatorFeedback : MonoBehaviour, IPointerClickHandler
+public class OnClickFeedback : MonoBehaviour, IPointerClickHandler
 {
     public void OnPointerClick(PointerEventData eventData)
     {
