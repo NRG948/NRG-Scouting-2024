1ba9258211302b12a055b932013d36b9e290a6b5
101zh <67253838+101zh@users.noreply.github.com>
2024-03-09 09:33:18 -0800

    Add options for repeated Heavy & Light feedback

--- Assets/Scripts/HapticManager.cs
+++ Assets/Scripts/HapticManager.cs
@@ -8,7 +8,8 @@ public class HapticManager : MonoBehaviour
     // Start is called before the first frame update
 
 
-    public static void LightFeedback() {
+    public static void LightFeedback()
+    {
         Debug.Log("Light");
         if (!(SystemInfo.supportsVibration) || PlayerPrefs.GetInt("Haptic") == 0) { return; }
         HapticFeedback.LightFeedback();
@@ -16,7 +17,7 @@ public class HapticManager : MonoBehaviour
 
     public static void LightFeedback(string location)
     {
-        Debug.Log("Light"+location);
+        Debug.Log("Light" + location);
         if (!(SystemInfo.supportsVibration) || PlayerPrefs.GetInt("Haptic") == 0) { return; }
         HapticFeedback.LightFeedback();
     }
@@ -28,9 +29,9 @@ public class HapticManager : MonoBehaviour
         HapticFeedback.HeavyFeedback();
     }
 
-    /// <summary>
-    /// Repeats HeavyFeedback indefinetly; must call StopCoroutine() to stop
-    /// </summary>
+    /** 
+     * <summary> Repeats HeavyFeedback indefinitely; must call StopCoroutine() to stop </summary>
+    */
     public static IEnumerator RepeatedHeavyFeedback()
     {
         while (true)
@@ -40,15 +41,43 @@ public class HapticManager : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Repeats LightFeedback indefinetly; must call StopCoroutine() to stop
-    /// </summary>
+    /** 
+     * <summary> Repeats LightFeedback indefinitely; must call StopCoroutine() to stop </summary>
+    */
     public static IEnumerator RepeatedLightFeedback()
     {
         while (true)
         {
             LightFeedback();
             yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    /** 
+     * <summary> Repeats LightFeedback <paramref name="x"/> times every <paramref name="timeInterval"/> seconds </summary>
+     * <param name="x"> times LightFeedback repeats </param>
+     * <param name="timeInterval"> time interval between repeats </param>
+    */
+    public static IEnumerator RepeatedLightFeedback(int x = 1, float timeInterval = 0.3f)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            LightFeedback();
+            yield return new WaitForSeconds(timeInterval);
+        }
+    }
+
+    /** 
+    * <summary> Repeats HeavyFeedback <paramref name="x"/> times every <paramref name="timeInterval"/> seconds </summary>
+    * <param name="x"> times HeavyFeedback repeats </param>
+    * <param name="timeInterval"> time interval between repeats </param>
+    */
+    public static IEnumerator RepeatedHeavyFeedback(int x = 1, float timeInterval = 0.3f)
+    {
+        for (int i = 0; i < x; i++)
+        {
+            HeavyFeedback();
+            yield return new WaitForSeconds(timeInterval);
         }
     }
 
