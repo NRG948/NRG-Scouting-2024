7a12f4d370f42b2f65dd65eb0df269c6c555fea9
101zh <67253838+101zh@users.noreply.github.com>
2024-03-09 16:52:58 -0800

    did more tagging in obj scout

--- Assets/Scripts/Theme Manager.cs
+++ Assets/Scripts/Theme Manager.cs
@@ -9,27 +9,28 @@ public class ThemeManager : MonoBehaviour
 {
     public bool darkMode;
     public bool pinkMode;
-    private Color darkRed = new Color(1f, 0.435849f, 0.4986054f, 1f);
-    private Color darkYellow = new Color(1f, 0.8766348f, 0.2396226f, 1f);
-    private Color darkGrey = new Color(0.6269776f, 0.7433963f, 0.719469f, 1f);
-    private Color darkBlue = new Color(0.6588235f, 0.9276033f, 0.9921569f, 1f);
-    private Color white = new Color(1f, 1f, 1f, 1f);
-    private Color offWhite = new Color(0.95f, 0.95f, 0.95f, 1f);
-    private Color greyishWhite = new Color(0.85f, 0.85f, 0.85f, 1f);
-    private Color darkBackground = new Color(0.04348518f, 0.03633317f, 0.07924521f, 1f);
-    private Color darkBackgroundBox = new Color(0.2215663f, 0.2329649f, 0.2603773f, 1f);
+    private readonly Color darkRed = new Color(1f, 0.435849f, 0.4986054f, 1f);
+    private readonly Color darkYellow = new Color(1f, 0.8766348f, 0.2396226f, 1f);
+    private readonly Color darkGrey = new Color(0.6269776f, 0.7433963f, 0.719469f, 1f);
+    private readonly Color darkBlue = new Color(0.6588235f, 0.9276033f, 0.9921569f, 1f);
+    private readonly Color white = new Color(1f, 1f, 1f, 1f);
+    private readonly Color offWhite = new Color(0.95f, 0.95f, 0.95f, 1f);
+    private readonly Color greyishWhite = new Color(0.85f, 0.85f, 0.85f, 1f);
+    private readonly Color lightGrey = new Color(0.75f, 0.75f, 0.75f, 1f);
+    private readonly Color darkBackground = new Color(0.04348518f, 0.03633317f, 0.07924521f, 1f);
+    private readonly Color darkBackgroundBox = new Color(0.2215663f, 0.2329649f, 0.2603773f, 1f);
 
-    private Color hotPink = new Color(1f, 0.15686275f, 0.521156863f, 1f);
-    private Color darkPink = new Color(0.81568627f, 0.007874314f, 0.35686275f, 1f);
-    private Color lightPink = new Color(1f, 0.43137255f, 0.79215686f, 1f);
-    private Color pinkRed = new Color(1f, 0f, 0.11111111111f, 1f);
-    private Color pinkYellow = new Color(0.9725491f, 0.8588236f, 0.08235294f);
-    private Color pinkGrey = new Color(0.5176471f, 0.5529412f, 0.5568628f, 1f);
-    private Color pinkBlue = new Color(0.121885f, 0.4774481f, 0.7830189f, 1f);
-    private Color lighterPink = new Color(1f, 0.567f, 1f, 1f);
-    private Color red = new Color(1f, 0f, 0f, 1f);
-    private Color blue = new Color(0f, 0.48f, 1f, 1f);
-    private Color orange = new Color(1f, 0.7267137f, 0f, 1f);
+    private readonly Color hotPink = new Color(1f, 0.15686275f, 0.521156863f, 1f);
+    private readonly Color darkPink = new Color(0.81568627f, 0.007874314f, 0.35686275f, 1f);
+    private readonly Color lightPink = new Color(1f, 0.43137255f, 0.79215686f, 1f);
+    private readonly Color pinkRed = new Color(1f, 0f, 0.11111111111f, 1f);
+    private readonly Color pinkYellow = new Color(0.9725491f, 0.8588236f, 0.08235294f);
+    private readonly Color pinkGrey = new Color(0.5176471f, 0.5529412f, 0.5568628f, 1f);
+    private readonly Color pinkBlue = new Color(0.121885f, 0.4774481f, 0.7830189f, 1f);
+    private readonly Color lighterPink = new Color(1f, 0.567f, 1f, 1f);
+    private readonly Color red = new Color(1f, 0f, 0f, 1f);
+    private readonly Color blue = new Color(0f, 0.48f, 1f, 1f);
+    private readonly Color orange = new Color(1f, 0.7267137f, 0f, 1f);
 
 
 
@@ -62,7 +63,8 @@ public class ThemeManager : MonoBehaviour
             ColorAllWithTag("BackgroundDesign", darkBackground);
             ColorAllWithTag("ContentText", white);
             ColorAllWithTag("TextOnColor", offWhite);
-
+            ColorAllWithTag("InputField", darkGrey);
+            ColorAllWithTag("SecondInputField", darkGrey);
         }
         if (pinkMode)
         {
