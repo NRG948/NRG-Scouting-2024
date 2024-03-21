59c89401f57ecacdfc5ccf8f42356f4ce4005dfe
Zeen_Z <zeen1127.zz@gmail.com>
2024-03-20 20:54:46 -0700

    Change gamemode string when changing gamemode.
    Co-Authored-By: Hunter Zhuang <67253838+101zh@users.noreply.github.com>

---
+++ Assets/Scripts/TeamGuesser/GameModeController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameModeController : MonoBehaviour
+{
+    ToggleGroup toggleGroup;
+    GameManager gameManager;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        toggleGroup = GetComponent<ToggleGroup>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    public void onChangeMode(string mode)
+    {
+        gameManager.gamemode = mode;
+    }
+}
